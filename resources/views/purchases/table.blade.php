092f4bdb5fc11b13ed34814cdec3f198aa74404c
rifalpg <rifalpramadita@gmail.com>
2017-12-15 08:46:50 +0700

    remove action

--- resources/views/purchases/table.blade.php
+++ resources/views/purchases/table.blade.php
@@ -1,12 +1,11 @@
 <table class="table table-responsive" id="purchases-table">
     <thead>
-        <tr>
-            <th>Code</th>
+    <tr>
+        <th>Code</th>
         <th>Ref</th>
         <th>Amount</th>
         <th>Total</th>
-            <th colspan="3">Action</th>
-        </tr>
+    </tr>
     </thead>
     <tbody>
     @foreach($purchases as $purchase)
@@ -15,15 +14,6 @@
             <td>{!! $purchase->ref !!}</td>
             <td>{!! $purchase->amount !!}</td>
             <td>{!! $purchase->total !!}</td>
-            <td>
-                {!! Form::open(['route' => ['purchases.destroy', $purchase->id], 'method' => 'delete']) !!}
-                <div class='btn-group'>
-                    <a href="{!! route('purchases.show', [$purchase->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                    <a href="{!! route('purchases.edit', [$purchase->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                    {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
-                </div>
-                {!! Form::close() !!}
-            </td>
         </tr>
     @endforeach
     </tbody>
